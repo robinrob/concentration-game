c51f82d5f60320973f62d64dae9485fa2cd9216f
Robin Smith <robin@mrrobinsmith.com>
2015-03-01 11:32:10 +0000

    Working tiles

--- src/ui.js
+++ src/ui.js
@@ -1,36 +1,31 @@
+var colors = {
+    yellow: new cc.color(255, 255, 0, 255),
+    green: new cc.color(0, 255, 0, 255),
+    purple: new cc.color(174, 0, 255, 255),
+    pink: new cc.color(255, 0, 255, 255),
+    orange: new cc.color(255, 78, 0, 255),
+    maroon: new cc.color(172, 6, 84, 255),
+    brown: new cc.color(145, 58, 6, 255),
+    blue: new cc.color(6, 87, 234, 255)
+}
+
+var tileColors = [
+    colors.yellow,
+    colors.green,
+    colors.purple,
+    colors.pink,
+    colors.orange,
+    colors.maroon,
+    colors.brown,
+    colors.blue
+]
+
 var ui = {
-    tileColors: [
-        new cc.color(255, 255, 0, 255),
-        new cc.color(0, 0, 255, 255),
-        new cc.color(255, 0, 255, 255),
-        new cc.color(255, 0, 198, 255),
-        new cc.color(255, 78, 0, 255),
-        new cc.color(172, 6, 84, 255),
-        new cc.color(145, 58, 6, 255),
-        new cc.color(6, 87, 234, 255)
-    ],
-    tileN: null,
     coverTile: function() {
         return new CompositeSprite(["assets/tile_bg.png", "assets/tile_cover.png"]);
     },
 
-    tileN: function(n) {
-        return new CompositeSprite(["assets/tile_bg.png", "assets/tile_" + n + ".png"]).setCompColor(n, tileColors[n])
-    },
-
-    tile0: function() {
-        return this.tileN(0).setCompColor(0, new cc.color(255, 255, 0, 255))
-    },
-
-    tile1: function() {
-        return this.tileN(1).setCompColor(0, new cc.color(0, 0, 255, 255))
-    },
-
-    tile2: function() {
-        return this.tileN(2).setCompColor(0, new cc.color(255, 0, 255, 255))
-    },
-
-    tile3: function() {
-        return this.tileN(2).setCompColor(0, new cc.color(255, 0, 255, 255))
+    tile: function(n) {
+        return new CompositeSprite(["assets/tile_bg.png", "assets/tile_" + n + ".png"]).setCompColor(0, tileColors[n])
     }
 }
