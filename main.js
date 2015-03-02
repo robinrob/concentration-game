231c50bac9e1cb9e5cdea71dba11f1418e593d71
Robin Smith <robin@mrrobinsmith.com>
2015-03-02 19:56:40 +0000

    Update device display options

--- main.js
+++ main.js
@@ -48,7 +48,9 @@
  */
 
 cc.game.onStart = function(){
+    cc.view.adjustViewPort(true);
     cc.view.setDesignResolutionSize(320, 480, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
     cc.LoaderScene.preload(mrrobinsmith.resources, function () {
         cc.director.runScene(new MenuScene());
     }, this);
