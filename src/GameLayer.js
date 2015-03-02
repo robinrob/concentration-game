84f50603d6086f943c93d3a5291706aa9a9a17d3
Robin Smith <robin@mrrobinsmith.com>
2015-03-02 18:10:18 +0000

    Finished and refactored working game

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,17 +1,31 @@
 var GameLayer = cc.Layer.extend({
-    nTiles: null,
+    _gameArray: [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7],
+    gameArray: null,
+    margin: 49,
+    gap: 10,
+    fontSize: 32,
+
     tiles: null,
-    pairs: null,
     turned: null,
     listener: null,
+    moves: null,
+    scoreLabel: null,
 
     ctor: function() {
         this._super()
 
         this._className = "GameLayer"
-
         this.tiles = []
 
+        this._constructListener()
+        this._constructBackground()
+        this._constructRestartMenu()
+        this._constructStats()
+
+        this.init()
+    },
+
+    _constructListener: function() {
         var that = this
         this.listener = cc.EventListener.create({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
@@ -26,29 +40,39 @@ var GameLayer = cc.Layer.extend({
                 }
             }
         })
-        var gradient = cc.LayerGradient.create(cc.color(0,0,0,255), cc.color(0x46,0x82,0xB4,255));
-        this.addChild(gradient, 0);
+    },
 
+    _constructRestartMenu: function() {
         var winSize = cc.director.getWinSize();
         var centerPos = cc.p(winSize.width / 2, winSize.height / 6 );
 
         var menuItemPlay = new cc.MenuItemSprite(
             ui.restartButton().normal,
             ui.restartButton().selected,
-            this.reset, this);
+            this.init, this);
         var menu = new cc.Menu(menuItemPlay);
         menu.setPosition(centerPos);
         this.addChild(menu, 2);
-
-        this.reset()
     },
 
-    reset: function() {
-        //this.gameArray = [0,0]
-        this.gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7]
-        this.nTiles = this.gameArray.length
+    _constructBackground: function() {
+        var gradient = cc.LayerGradient.create(cc.color(0,0,0,255), cc.color(0x46,0x82,0xB4,255));
+        this.addChild(gradient, 0);
+    },
+
+    _constructStats: function() {
+        var winSize = cc.director.getWinSize();
+
+        var lbl = cc.LabelTTF.create("","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+        this.addChild(lbl)
+        lbl.setPosition(this.fontSize + this.margin, winSize.height - 25)
+        this.scoreLabel = lbl
+    },
+
+    init: function() {
+        this.gameArray = this._gameArray.concat()
         this.turned = []
-        this.pairs = 0
+        this.setMoves(0)
 
         var that = this
         this.tiles.forEach(function(tile) {
@@ -56,13 +80,33 @@ var GameLayer = cc.Layer.extend({
         })
 
         this.tiles = []
-        for(i = 0; i < this.nTiles; i++){
+        var nTiles = this.gameArray.length
+        for(i = 0; i < nTiles; i++){
             var tile = new Tile(this.takeRandom(this.gameArray))
             cc.eventManager.addListener(this.listener.clone(), tile);
             this.tiles.push(tile)
             this.addChild(tile, 1);
-            tile.setPosition(49+i%4*74,400-Math.floor(i/4)*74);
+
+            var x = this.margin + ((i % 4) * (64 + this.gap))
+            var y = 400 - (Math.floor(i / 4) * (64 + this.gap))
+            tile.setPosition(x, y);
         }
+    },
+
+    setMoves: function(moves) {
+        this.moves = moves
+        this.scoreLabel.setString("Moves: "+moves);
+    },
+
+    incMoves: function() {
+      this.setMoves(this.moves + 1)
+    },
+
+    takeRandom: function(items) {
+        var index = Math.round(Math.random() * (items.length - 1))
+        var item = items[index]
+        items.splice(index, 1)
+        return item
     },
 
     checkTiles: function(tile) {
@@ -73,6 +117,7 @@ var GameLayer = cc.Layer.extend({
             this.turned.push(tile)
 
             if (this.turned.length == 2) {
+                this.incMoves()
                 var pause = setTimeout(function () {
                     that.checkMatch()
                 }, 1000)
@@ -101,20 +146,5 @@ var GameLayer = cc.Layer.extend({
         }, that)
 
         this.turned = []
-    },
-
-    isChecking: function() {
-        return this._checking
-    },
-
-    setIsChecking: function(bool) {
-        this._checking = bool
-    },
-
-    takeRandom: function(items) {
-        var index = Math.round(Math.random() * (items.length - 1))
-        var item = items[index]
-        items.splice(index, 1)
-        return item
     }
 });
--- src/ui.js
+++ src/ui.js
@@ -7,27 +7,10 @@ var ui = {
         return new CompositeSprite(["assets/tile_bg.png", "assets/tile_" + n + ".png"]).setCompColor(0, tileColors[n])
     },
 
-    touchListener: function(callBack, that) {
-        var listener = cc.EventListener.create({
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            swallowTouches: true,
-            onTouchBegan: function (touch, event) {
-                var target = event.getCurrentTarget();
-                var location = target.convertToNodeSpace(touch.getLocation());
-                var targetSize = target.getContentSize();
-                var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
-                if (cc.rectContainsPoint(targetRectangle, location)) {
-                    callBack(target).apply(that)
-                }
-            }
-        })
-        return listener
-    },
-
     restartButton: function() {
         var buttons = {
             normal:(function () {
-                var btn = new CompositeSprite([mrrobinsmith.res.button_outer_png, mrrobinsmith.res.button_n_inner_png, mrrobinsmith.res.restart_n_text_png])
+                var btn = new CompositeSprite([mrrobinsmith.res.button_outer_png,mrrobinsmith.res.button_n_inner_png, mrrobinsmith.res.restart_n_text_png])
                 btn.setColor(mrrobinsmith.g.buttonOuterColor)
                 btn.setChildColor(0, mrrobinsmith.g.buttonInnerColor)
                 btn.setChildColor(1, mrrobinsmith.g.buttonTextColor)
